a83080c1b587ecd4001610fa784bc69411f39b0f
ShivAiML <soni.shivmohan@gmail.com>
2025-06-08 18:21:17 +0530

    Add files via upload

---
+++ Assignment1.py
@@ -0,0 +1,16 @@
+#Takes two numbers as input from the user.
+num1 = float(input("Enter first number:"))
+num2 = float(input("Enter second number:"))
+
+#Performing actions
+
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+
+#display the result
+print("Addition =",addition)
+print("Subtraction =",subtraction)
+print("Multiplication =",multiplication)
+print("Division =",division)
---
+++ assignment2.py
@@ -0,0 +1,9 @@
+#Personalize greeting
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+#Concatinate the name
+full_name = first_name + " " + last_name
+
+#result
+print(f"\nHello, {full_name} ! Welcome to the python program.")
